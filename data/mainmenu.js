595d558ebdef482af950ef8f9426ba692288d013
John F <87107510+EpicJohn11@users.noreply.github.com>
2022-12-14 09:06:33 -0600

    Update mainmenu.js

--- data/mainmenu.js
+++ data/mainmenu.js
@@ -1,14 +1,19 @@
 function PlayGame()
 {
-  
+  window.location.replace("data/levels/level01.html"); 
 }
 
 function OpenCredits()
 {
-  
+  window.location.replace("data/credits.html");
 }
 
 function QuitGame()
 {
   window.close(); 
+}
+
+function BackToMenu()
+{
+  window.location.replace("index.html"); 
 }
